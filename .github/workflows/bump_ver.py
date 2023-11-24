79c61efd949d6a6b02d1daa65f200a7e17578b65
ZapSNH <134786889+zapSNH@users.noreply.github.com>
2023-11-24 12:24:13 +0800

    sync zcpt and hopefully automate versioning

---
+++ .github/workflows/bump_ver.py
@@ -0,0 +1,37 @@
+import re
+import sys
+
+def bump(new_ver_v):
+	new_ver = new_ver_v.replace("v", "")
+	manifest_template = "\"version\": \"" + new_ver + "\""
+	updates_template = ''',
+        {
+          "version": "''' + new_ver + '''",
+          "update_link": "https://github.com/zapSNH/zcpt-webextension/releases/download/''' + new_ver + '''/zapsCoolPhotonTheme.xpi",
+          "applications": {
+            "gecko": {
+                "strict_min_version": "121.0a1"
+            }
+          }
+        }
+      ]
+    }
+  }
+}'''
+	replace(manifest_template, "manifest.json", "\"version\": \".*\"")
+	replace(updates_template, "updates.json", "\n.*]\n.*}\n.*}\n.*}")
+
+
+def replace(new_ver, file, regex):
+	f = open(file, "r")
+
+	content = f.read()
+	new_content = re.sub(regex, new_ver, content)
+
+	f.close()
+	f = open(file, "w")
+	f.write(new_content)
+	f.close()
+	
+if __name__ == '__main__':
+    globals()[sys.argv[1]](sys.argv[2])
