e586809c197ba02b8173928275951393a6e6168e
Zap <134786889+zapSNH@users.noreply.github.com>
2023-12-19 19:28:30 +0800

    test

--- .github/workflows/bump_ver.py
+++ .github/workflows/bump_ver.py
@@ -4,7 +4,10 @@ import sys
 def bump(new_ver_v):
 	new_ver = new_ver_v.replace("v", "")
 	manifest_template = "\"version\": \"" + new_ver + "\""
-	updates_template = ''',
+	updates_template = '''{
+  "addons": {
+    "coolPhotonTheme@zapsnh": {
+      "updates": [
         {
           "version": "''' + new_ver + '''",
           "update_link": "https://github.com/zapSNH/zcpt-webextension/releases/download/v''' + new_ver + '''/zapsCoolPhotonTheme.xpi",
@@ -13,14 +16,24 @@ def bump(new_ver_v):
                 "strict_min_version": "115.0"
             }
           }
+        },
+        {
+          "version": "''' + new_ver + '''.1",
+          "update_link": "https://github.com/zapSNH/zcpt-webextension/releases/download/v0''' + new_ver + '''.1/zapsCoolPhotonTheme.xpi",
+          "applications": {
+            "gecko": {
+                "strict_min_version": "116.0"
+            }
+          }
         }
       ]
     }
   }
-}'''
+}
+'''
 	versioncss_template = "content: \"zapsCoolPhotonTheme v" + new_ver + " (webextension)\";"
 	replace(manifest_template, "manifest.json", "\"version\": \".*\"")
-	replace(updates_template, "updates.json", "\n.*]\n.*}\n.*}\n.*}")
+	replaceAll(updates_template, "updates.json")
 	replace(versioncss_template, "custom/resources/version.css", "content: \"zapsCoolPhotonTheme v.* \(webextension\)\";")
 
 
@@ -35,5 +48,12 @@ def replace(new_ver, file, regex):
 	f.write(new_content)
 	f.close()
 	
+def replaceAll(new_ver, file):
+	new_content = new_ver
+	
+	f = open(file, "w")
+	f.write(new_content)
+	f.close()
+
 if __name__ == '__main__':
     globals()[sys.argv[1]](sys.argv[2])
